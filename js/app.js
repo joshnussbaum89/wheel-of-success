d6a31c65a8d1ab229d9c40c088c281d4997dc1c0
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-08-06 17:29:43 -0400

    troubleshoot addPhraseToDisplay bug

--- js/app.js
+++ js/app.js
@@ -22,17 +22,45 @@ const phrases = [
 // randomly choose movie quote from phrases array
 const getRandomPhraseAsArray = arr => {
 
-    //randomly choose a phrase from phrases array
-    const randomPhrase = Math.floor(Math.random() * phrases.length);
+    //randomly choose a phrase index 
+    const randomNumber = Math.floor(Math.random() * phrases.length);
 
     //return the array element at random index calculated 
-    const phraseAtIndex = phrases[randomPhrase];
+    const randomPhrase = phrases[randomNumber];
 
     //split that phrase into new array of characters
-    const phraseAsCharacters = phraseAtIndex.split(', ')
+    const phraseAsCharacters = randomPhrase.split('');
 
     //return new character array
     return phraseAsCharacters;
 };
 
-console.log(getRandomPhraseAsArray(phrases));
+const phraseArray = getRandomPhraseAsArray(phrases);
+
+//set the game display
+const addPhraseToDisplay = arr => {
+
+    //loop through array of characters
+    for (let i = 0; i < phraseArray.length; i++) {
+
+        //for each character, create li, put character in li
+        let li = phraseArray[i];
+        let liPhrase = document.createElement('li');
+        liPhrase.textContent = li.value;
+
+        //append to #phrase ul
+        const ul = document.querySelector('#phrase ul');
+        ul.appendChild(liPhrase);
+
+        //if character in array is a letter, add the class, 'letter' to li
+        if (li) {
+            liPhrase.classList.add('.letter');
+            //character === " " add the class, 'space' to li
+        } else if (li === " ") {
+            liPhrase.classList.add('.space');
+        }
+    }
+
+}
+
+addPhraseToDisplay(phraseArray);
