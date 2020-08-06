ba9ab16e0d7b8b81785c22eedf1a9d5f9068b214
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-08-06 18:20:31 -0400

    fix addPhraseToDisplay func

--- js/app.js
+++ js/app.js
@@ -12,55 +12,54 @@ startGameButton.addEventListener('click', () => {
 
 // movie quote array 
 const phrases = [
-    'the body cannot live without the mind', //the matrix
-    'im in a glass case of emotion', //anchorman 
-    'i dont think were in kansas anymore', //the wizard of oz
-    'hold on to your butts', //jurassic park
-    'come with me if you want to live' //terminator 2
+    'the body cannot live without the mind', // the matrix
+    'im in a glass case of emotion', // anchorman 
+    'i dont think were in kansas anymore', // the wizard of oz
+    'hold on to your butts', // jurassic park
+    'come with me if you want to live' // terminator 2
 ]
 
 // randomly choose movie quote from phrases array
 const getRandomPhraseAsArray = arr => {
 
-    //randomly choose a phrase index 
+    // randomly choose a phrase index 
     const randomNumber = Math.floor(Math.random() * phrases.length);
 
-    //return the array element at random index calculated 
+    // return the array element at random index calculated 
     const randomPhrase = phrases[randomNumber];
 
-    //split that phrase into new array of characters
+    // split that phrase into new array of characters
     const phraseAsCharacters = randomPhrase.split('');
 
-    //return new character array
+    // return new character array
     return phraseAsCharacters;
 };
 
 const phraseArray = getRandomPhraseAsArray(phrases);
 
-//set the game display
+// set the game display
 const addPhraseToDisplay = arr => {
 
-    //loop through array of characters
+    // loop through array of characters
     for (let i = 0; i < phraseArray.length; i++) {
 
-        //for each character, create li, put character in li
+        // for each character, create li, put character in li
         let li = phraseArray[i];
         let liPhrase = document.createElement('li');
-        liPhrase.textContent = li.value;
+        liPhrase.textContent = li;
 
-        //append to #phrase ul
+        // append to #phrase ul
         const ul = document.querySelector('#phrase ul');
         ul.appendChild(liPhrase);
 
-        //if character in array is a letter, add the class, 'letter' to li
-        if (li) {
-            liPhrase.classList.add('.letter');
-            //character === " " add the class, 'space' to li
+        // if character in array is a letter, add the class, 'letter' to li
+        // else add the class, 'space'
+        if (li !== " ") {
+            liPhrase.classList.add('letter');
         } else if (li === " ") {
-            liPhrase.classList.add('.space');
+            liPhrase.classList.add('space');
         }
     }
-
 }
 
 addPhraseToDisplay(phraseArray);
