c24e7004f640fff9f16a2170201f19d24f7f2459
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-08-07 12:07:58 -0400

    add checkLetter function and qwerty event listener

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const startGameButton = document.querySelector('.btn__reset');
+const button = document.querySelectorAll('button');
 let missed = 0;
 
 // remove start screen overlay with start game button
@@ -47,7 +48,6 @@ const addPhraseToDisplay = arr => {
         let li = phraseArray[i];
         let liPhrase = document.createElement('li');
         liPhrase.textContent = li;
-
         // append to #phrase ul
         const ul = document.querySelector('#phrase ul');
         ul.appendChild(liPhrase);
@@ -60,6 +60,46 @@ const addPhraseToDisplay = arr => {
             liPhrase.classList.add('space');
         }
     }
-}
+};
+
+// check if player guess match any letters
+const checkLetter = playerGuess => {
+    let checkLetter = document.querySelector('ul .letter');
+    // loop over the letters and check if they match the letter in the button the player has chosen
+    for (let i = 0; i < checkLetter.length; i++) {
+        // If there’s a match
+        if (playerGuess === checkLetter[i]) {
+            // add the “show” class to the list item containing that letter
+            // store the matching letter inside of a variable
+            let matchingLetter = checkLetter[i].classList.add('show');
+            // return that letter.
+            return matchingLetter;
+            // If a match wasn’t found, return null.
+        } else if (playerGuess !== checkLetter[i]) {
+            return null;
+        }
+    }
+};
+
+// click a letter, change slyle of selected button
+qwerty.addEventListener('click', (e) => {
+    // save button choice to variable
+    let buttonChoice = event.target;
+    // loop through buttons because querySelectorAll('button') creates list
+    for (let i = 0; i < button.length; i++) {
+        // if button choice equals one of the button options, change stlye of that button
+        if (buttonChoice === button[i]) {
+            buttonChoice.classList.add('chosen');
+            // disable that button
+            buttonChoice.disabled = true; // adds a glitch to the button transition...?
+        }
+    }
+
+    // CALL THE CHECKLETTER FUNCTION **************************************************
+
+    //  If the checkLetter function does not find a letter, remove one of the heart
+    //  images and increment the missed counter
+
+});
 
 addPhraseToDisplay(phraseArray);
